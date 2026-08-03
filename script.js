9a1eb73fa95d2d7fd4a46d67f93494ef7b13c0b3
nathaniel-knerr <knerrnathaniel@gmail.com>
2026-08-03 08:57:56 -0700

    Added Coaster Jump in and iframe for both games and created two additional pages for both games to be played

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("script.js running!");
+const playButtonCJ = document.getElementById("play-button-cj");
+
+playButtonCJ.addEventListener("click", () => {
+	window.location.href = "coaster-jump.html";
+});
+
+const playButtonTA = document.getElementById("play-button-ta");
+
+playButtonTA.addEventListener("click", () => {
+	window.location.href = "track-attack.html";
+});
